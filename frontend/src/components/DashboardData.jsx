bf35d870089521e1471e558ef075e2678dbc8ba9
Singh-Ishu <itzshaansingh@gmail.com>
2025-08-01 16:19:46 +0530

    ADD DashboardData Menu

---
+++ frontend/src/components/DashboardData.jsx
@@ -0,0 +1,14 @@
+import "./DashboardData.css";
+
+function DashboardData() {
+    return (
+        <div className="dashboard-data-container">
+            <div className="dashboard-data-value-holder">100</div>
+            <div className="dashboard-data-value-holder">30</div>
+            <div className="dashboard-data-value-holder">60</div>
+            <div className="dashboard-data-value-holder">10</div>
+        </div>
+    );
+}
+
+export default DashboardData;
