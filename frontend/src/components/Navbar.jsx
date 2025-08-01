aaa2c22178650676a16df42a428dca9e0574b535
Singh-Ishu <itzshaansingh@gmail.com>
2025-08-01 16:20:31 +0530

    ADD navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar-main">
+            <ul>
+                <li>Home</li>
+                <li>Cars</li>
+                <li>Slots</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
