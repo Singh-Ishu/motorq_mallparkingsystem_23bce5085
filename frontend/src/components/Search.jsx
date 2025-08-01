757a348b71f935dcdb1a98792fc53172673606fb
Singh-Ishu <itzshaansingh@gmail.com>
2025-08-01 16:39:01 +0530

    Add Search button

---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,11 @@
+import "./Search.css";
+
+function Search() {
+    return (
+        <div className="search-bar-dashboard">
+            <input></input>
+        </div>
+    );
+}
+
+export default Search;
