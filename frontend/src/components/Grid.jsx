28fad86c04a1f77980d259c599e28f859ffdf5a7
Singh-Ishu <itzshaansingh@gmail.com>
2025-08-01 16:20:01 +0530

    SETUP Basic Grid

---
+++ frontend/src/components/Grid.jsx
@@ -0,0 +1,24 @@
+import "./Grid.css";
+
+function Grid() {
+    return (
+        <div className="grid_dashboard">
+            GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+            <br /> GRID GOES HERE
+        </div>
+    );
+}
+
+export default Grid;
